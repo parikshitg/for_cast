fc7043c0a51bf5456cde1c12f6609fc1479dbd50
Parikshit Gothwal <parikshit.g@gopherslab.com>
2021-07-31 12:44:05 +0530

    feat: initialized homescreen and assets added

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ForCast', style: TextStyle(fontSize: 28, fontFamily: 'Ethnocentric',),),
+      ),
+    );
+  }
+}
